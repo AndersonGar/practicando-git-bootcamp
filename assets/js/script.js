436fd9ea8c8bfed2cd6c021b6a2a9ade20771e9f
bella <bpacheco@estudiantes.crackthecode.la>
2021-11-22 08:18:53 -0600

    bella

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn-validar").addEventListener("click",function(){ 
+    var inputNumberCard = document.getElementById ("numero-tarjeta").value;
+    alert(input);
+})
+    
