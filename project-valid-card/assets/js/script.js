a0c35d107f8a27913baa5e236a0e486e1b8c687e
eliza <elizivillar3llar@gmail.com>
2021-11-22 09:09:38 -0500

    captura del valor del input

---
+++ project-valid-card/assets/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-validar").addEventListener("click",function(){
+    var inputnumberCard = document.getElementById("numero-tarjeta").value;
+    alert(inputnumberCard);
+})
