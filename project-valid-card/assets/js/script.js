98a23145a5594f18615c7c2ebf6491831f888805
Yanira <jmonge@estudiantes.crackthecode.la>
2021-11-23 09:58:34 -0500

    terminando las tarjetas de credito validas

--- project-valid-card/assets/js/script.js
+++ project-valid-card/assets/js/script.js
@@ -1,4 +1,26 @@
 document.getElementById("btn-validar").addEventListener("click",function(){
+    //obtenemos el string del input
     var inputNumberCard = document.getElementById("numero-tarjeta").value;
-    alert(inputNumberCard)
+    //variable para saber el total
+    var suma = 0;
+    //buble para obtener cada numero con su valor
+    for(i=0;i<inputNumberCard.length;i++){
+        if(i%2==0 || i == 0){
+            var n = parseInt(inputNumberCard[i]);
+            n = n*2;
+            if(n>9){
+                r = n%10;
+                n = r + 1; 
+            }
+        }
+        //actualizar la suma total de cada digito procesado
+            suma +- n;
+           
+    }
+    var mensaje = document.getElementById("alerta");
+    mensaje.innerHTML = "el numero de la tarjeta es invalido"
+    if(suma%10 == 0){
+        mensaje.innerHTML = "El número de la tarjeta es valido"
+            
+    }
 })
