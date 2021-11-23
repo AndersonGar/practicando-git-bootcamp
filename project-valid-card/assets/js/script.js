6cec7ad98fde2d9b5ad13c7505115089ad87ad1c
Aesthetic <ssebastian@estudiantes.crackthecode.la>
2021-11-23 10:09:25 -0600

    borrador

--- project-valid-card/assets/js/script.js
+++ project-valid-card/assets/js/script.js
@@ -1,4 +1,28 @@
 document.getElementById("btn-validar").addEventListener("click",function(){
  var inputNumberaCard = document.getElementById("numero-tarjeta").value;
- alert(inputNumberaCard);
+var suma =0;
+   for(i=0;i<inputNumberaCard.length;i++){
+   //
+    var n = parseInt(inputNumberaCard[i]);
+    //condicional para saber si se debe operar o no
+   if(i%2==0 || i == 0 ){
+       n= n*2;
+       //condicional para saber si el numero tiene dos digitos
+       if(n>9){
+       //obtener el segundo digito
+          r = n%10;
+       //sumar lo digitos
+           n = r + 1;
+        }
+    }
+    //Actualizar la suma total de cada digito  procesado
+    suma += n;
+   }
+   alert(suma);
+   var mensaje =document.getElementById("alerta");
+   mensaje.innerHTML ="El numero de la tarjeta invalido";
+   if(suma%10 == 0){
+       mensaje.innerHTML ="El numero de la tarjeta es valido";
+   }
+
 })
