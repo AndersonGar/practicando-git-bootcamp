6fe42b9b921e6e24aa04127d62babe49d847f41f
Yanira <jmonge@estudiantes.crackthecode.la>
2021-11-25 08:33:32 -0500

    AvanzeUwu

--- project-valid-card/assets/js/script.js
+++ project-valid-card/assets/js/script.js
@@ -24,3 +24,28 @@ document.getElementById("btn-validar").addEventListener("click",function(){
             
     }
 })
+//Evento de click en el boton con el id 'btn-cambiar'
+document.getElementById("btn-cambiar").addEventListener("click",function(){
+    //Variable para guardar el valor del input
+    var mensaje = document.getElementById("msj").value;
+    //Arreglo vacío para los nuevas códigos
+    var codigosNuevos = [];
+    //Creamos una variable para el nuevo mensaje
+    var nuevoMensaje = "";
+    //Bucle para recorrer las letras de la palabra
+    for(i=0;i<mensaje.length ;i++){
+        var c = mensaje[i].toLowerCase();
+        //Variable para guardar el número de la posición de la letra del mensaje
+        var codMsj = c.charCodeAt();
+        //Variable para obtener el nuevo código de mi nueva letra
+        var codNuevo = codMsj + 2;
+         if(codNuevo > 122){
+             var d = codNuevo - 122;
+             codNuevo = 96 + d;
+         }
+        //Agregar el nuevo codigo al arreglo
+        codigosNuevos.push(codNuevo);
+        //Concatenar las nuevas letras en un solo string, la variable nuevoMensaje
+        nuevoMensaje;
+    }
+})
