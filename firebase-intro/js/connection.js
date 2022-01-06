60fc33b991d26b01c4254871cdcec321c179ef4a
Anderson <andersongarciavarillas@gmail.com>
2022-01-06 10:43:14 -0500

    Reading and Writing with Firebase/firestore

--- firebase-intro/js/connection.js
+++ firebase-intro/js/connection.js
@@ -1,14 +1,12 @@
-
-
-$(document).ready(function (){
+$(document).ready(function () {
     $("#registro").hide();
     $("#content").hide();
 
-    $("#btn-register").click(function(){
+    $("#btn-register").click(function () {
         $("#login-container").hide();
         $("#registro").show();
     })
-    
+
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     const firebaseConfig = {
@@ -28,26 +26,26 @@ $(document).ready(function (){
     const auth = firebase.auth();
 
     //Login o inicio de sesión
-    $("#btn-login").click(function(e){
+    $("#btn-login").click(function (e) {
         e.preventDefault();
         //Variables de inputs
         var correo = $("#mail").val();
         var clave = $("#pass").val();
         //Usar servicio de login de firebase
         auth
-            .signInWithEmailAndPassword(correo,clave)
-            .then(userCredential=>{
+            .signInWithEmailAndPassword(correo, clave)
+            .then(userCredential => {
                 alert("Datos correctos");
             })
-            .catch((error) =>{
+            .catch((error) => {
                 let errorCode = error.code;
                 let errorMessage = error.message;
-                alert("Código:"+errorCode+". Mensaje: "+errorMessage);
+                alert("Código:" + errorCode + ". Mensaje: " + errorMessage);
             })
     })
 
     //Singup o crear cuenta    
-    $("#btn-singup").click(function(e){
+    $("#btn-singup").click(function (e) {
         e.preventDefault();
         //Variables de inputs
         var correo = $("#mail-new").val();
@@ -55,16 +53,16 @@ $(document).ready(function (){
 
         //Usar servicio de firebase para crear cuenta
         auth
-            .createUserWithEmailAndPassword(correo,clave)
-            .then((userCredential)=>{
-                $("#login-container").show();
-                $("#registro").hide();
+            .createUserWithEmailAndPassword(correo, clave)
+            .then((userCredential) => {
+                //$("#login-container").show();
+                //$("#registro").hide();
                 alert("Cuenta creada");
             })
-            .catch((error)=>{
+            .catch((error) => {
                 let errorCode = error.code;
                 let errorMessage = error.message;
-                alert("Código:"+errorCode+". Mensaje: "+errorMessage);
+                alert("Código:" + errorCode + ". Mensaje: " + errorMessage);
             })
     })
 
@@ -74,34 +72,83 @@ $(document).ready(function (){
         e.preventDefault();
         auth.signOut().then(() => {
             alert("Sesión cerrada");
-            $("#content").hide();
-            $("#login-container").show();
+            //$("#content").hide();
+            //$("#login-container").show();
         })
     })
     var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
     //Iniciar sesión con Google
-    $("#btn-login-google").click(function(e){
+    $("#btn-login-google").click(function (e) {
         e.preventDefault();
         auth.signInWithPopup(provider)
-        .then(result => {
-            alert("Ingreso con Google");
-        })
-        .catch(error =>{
-            alert(error);
-        }) 
+            .then(result => {
+                alert("Ingreso con Google");
+            })
+            .catch(error => {
+                alert(error);
+            })
     })
 
-    auth.onAuthStateChanged((user)=>{
-        if(user){
+    auth.onAuthStateChanged((user) => {
+        if (user) {
             //Sesión iniciada
             $("#login-container").hide();
+            $("#register").hide();
             $("#content").show();
+            readPosts();
         }
-        else{
+        else {
             //Sesión finalizada
+            $("#content").hide();
             $("#login-container").show();
         }
     })
+    const db = firebase.firestore();
+    //Publicar un nuevo estado
+    $("#btn-publish").click(function(e){
+        e.preventDefault();
+        let postText = $("#status-text").val();
+        let date = new Date();
+        db.collection("posts").add({
+            post: postText,
+            day: date.getDate(),
+            month: date.getMonth() + 1,
+            year: date.getFullYear(),
+        })
+        .then((docRef)=>{
+            alert("Estado publicado");
+            $("#status-text").val('');
+            readPosts();
+        })
+        .catch((error) =>{
+            alert(error);
+        })
+    })
+
+    function readPosts(){
+        db.collection("posts").get().then((posts)=>{
+            listPosts(posts.docs);
+        })
+    }
+
+    function listPosts(data){
+        var divContent = $("#post-feed");
+        divContent.empty(); 
+        if(data.length > 0){
+            let content = "";
+            data.forEach(document => {
+                let doc = document.data();
+                const divPost = `
+                    <div style='border:solid 2px;'>
+                        <p>${doc.post}</p><br>
+                        <span>Publicado el: ${doc.day}/${doc.month}/${doc.year}<span>
+                    </div>
+                    <hr>
+                `;
+                content += divPost;
+            });
+            divContent.append(content);
+        }
+    }
 
 })
